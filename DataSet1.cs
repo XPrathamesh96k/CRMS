c7c64e2ba371429256578151f2172b5b666be53f
XPrathamesh96k <gprathamesh010@gmail.com>
2025-07-14 16:21:22 +0530

    Add files via upload

---
+++ DataSet1.cs
@@ -0,0 +1,23 @@
+﻿namespace CRMS
+{
+
+
+    partial class DataSet1
+    {
+        partial class DriverInvoiceDataTable
+        {
+        }
+
+        partial class DataTable2DataTable
+        {
+        }
+
+        partial class InvoiceCustomerPaymentDataTable
+        {
+        }
+
+        partial class CustomerDataTable
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRMS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            // Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new Report2.CustomerInvoice());
+            Application.Run(new Admin.Login_New());
+           // Application.Run(new Report2.DriverInvoice());
+        }
+    }
+}
